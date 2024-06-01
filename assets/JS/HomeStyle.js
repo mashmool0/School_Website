cb42ffe4060c90c5a03b3e7fd34f47d349cd2554
mani <manifallah052@gmail.com>
2024-06-01 12:26:51 +0330

    Home Fixed

--- assets/JS/HomeStyle.js
+++ assets/JS/HomeStyle.js
@@ -7,6 +7,8 @@ function clickChecker(){
     for(let a=0 ; a<links.length ; a++){
       links[a].style.display = "block";
     }
+    document.querySelector(".cards").style.zIndex = "-1"
+    document.querySelector(".headerContent").style.zIndex = "-1"
     checker= true;
   }
 
@@ -17,6 +19,8 @@ function clickChecker(){
     for(let a=0 ; a<links.length ; a++){
       links[a].style.display = "none";
     }
+    document.querySelector(".cards").style.zIndex = "0"
+    document.querySelector(".headerContent").style.zIndex = "0"
     checker = false;
   }
 }
