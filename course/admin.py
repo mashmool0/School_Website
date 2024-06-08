19178165ab2206f04a153153899c146875fbe24f
mashmool0 <ap4games2017@gmail.com>
2024-06-08 19:19:27 +0330

    move course model from home app to course application

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from jalali_date import datetime2jalali, date2jalali
+from jalali_date.admin import ModelAdminJalaliMixin, StackedInlineJalaliMixin, TabularInlineJalaliMixin
+from .models import Course
+
 
 # Register your models here.
+@admin.register(Course)
+class FirstModelAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['course_name', 'price', "teacher", "date_starter", "course_type", 'get_created_jalali']
+
+    @admin.display(description='تاریخ ایجاد', ordering='created')
+    def get_created_jalali(self, obj):
+        return datetime2jalali(obj.created).strftime('%a, %d %b %Y %H:%M:%S')
--- home/admin.py
+++ home/admin.py
@@ -1,16 +1,1 @@
 from django.contrib import admin
-from jalali_date import datetime2jalali, date2jalali
-from jalali_date.admin import ModelAdminJalaliMixin, StackedInlineJalaliMixin, TabularInlineJalaliMixin
-from .models import Course
-
-
-@admin.register(Course)
-class FirstModelAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
-    list_display = ['course_name', 'price', "teacher", "date_starter", "course_type", 'get_created_jalali']
-
-    @admin.display(description='تاریخ ایجاد', ordering='created')
-    def get_created_jalali(self, obj):
-        return datetime2jalali(obj.created).strftime('%a, %d %b %Y %H:%M:%S')
-
-
-
