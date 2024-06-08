5a84008f6885307ad1a126cc5852021b99285d14
mani <manifallah052@gmail.com>
2024-06-09 01:42:02 +0330

    Panel & Courses Fixed

--- assets/JS/basket.js
+++ assets/JS/basket.js
@@ -1,42 +1,64 @@
 let dropCheck = false;
-
 function dropdown() {
-  const dropDownDivs = document.querySelectorAll(".dropDown div");
-  const notif = document.querySelector(".notif");
-
-  if (!dropCheck) {
-    dropDownDivs.forEach((div) => {
-      div.style.display = "block";
-    });
+  if (dropCheck == false) {
+    let links = document.querySelectorAll(".dropDown div");
+    for (let a = 0; a < links.length; a++) {
+      links[a].style.display = "block";
+    }
     document.querySelector(".dropDown").style.marginTop = "120px";
-    notif.style.marginTop = "120px";
+    document.querySelector(".notif").style.marginTop = "120px"
     dropCheck = true;
   } else {
-    dropDownDivs.forEach((div) => {
-      div.style.display = "none";
-    });
+    let links = document.querySelectorAll(".dropDown div");
+    for (let a = 0; a < links.length; a++) {
+      links[a].style.display = "none";
+    }
     document.querySelector(".dropDown").style.marginTop = "0";
-    notif.style.marginTop = "0";
+    document.querySelector(".notif").style.marginTop = "0"
     dropCheck = false;
   }
 }
-
 let dropCheck2 = false;
-
 function dropdown2() {
-  const mobDivs = document.querySelectorAll(".mob div");
-
-  if (!dropCheck2) {
-    mobDivs.forEach((div) => {
-      div.style.display = "block";
-    });
+  if (dropCheck2 == false) {
+    let links = document.querySelectorAll(".mob div");
+    for (let a = 0; a < links.length; a++) {
+      links[a].style.display = "block";
+    }
     document.querySelector(".mob").style.marginTop = "120px";
     dropCheck2 = true;
   } else {
-    mobDivs.forEach((div) => {
-      div.style.display = "none";
-    });
+    let links = document.querySelectorAll(".mob div");
+    for (let a = 0; a < links.length; a++) {
+      links[a].style.display = "none";
+    }
     document.querySelector(".mob").style.marginTop = "0";
     dropCheck2 = false;
   }
+}
+plusBtn = document.querySelectorAll(".PlusBtn");
+for(let i=0 ; i<plusBtn.length ; i++){
+  plusBtn[i].addEventListener("click" , function(){
+    if(plusBtn[i].nextElementSibling.style.display == "none"){
+      plusBtn[i].nextElementSibling.style.display ="block"
+    }
+    else{
+      p = plusBtn[i].nextElementSibling.innerHTML;
+      plusBtn[i].nextElementSibling.innerHTML = ++p;
+    }
+  })
+}
+
+MinusBtn = document.querySelectorAll(".MinusBtn");
+for(let i=0 ; i<MinusBtn.length ; i++){
+  MinusBtn[i].addEventListener("click" , function(){
+    p = plusBtn[i].nextElementSibling.innerHTML;
+    if(p!=1){
+      plusBtn[i].nextElementSibling.innerHTML = --p;
+    }
+    else if(p==1){
+      plusBtn[i].nextElementSibling.style.display = "none";
+
+    }
+  })
 }
---
+++ assets/JS/course.js
@@ -0,0 +1,22 @@
+let checker = false;
+function clickChecker(){
+  if(checker==false){
+    document.querySelector(".HamburgerMenu").style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+    let links = document.querySelectorAll(".SideLinks a");
+    
+    for(let a=0 ; a<links.length ; a++){
+      links[a].style.display = "block";
+    }
+    checker= true;
+  }
+
+  else{
+    
+    document.querySelector(".HamburgerMenu").style.background = "none";
+    let links = document.querySelectorAll(".SideLinks a");
+    for(let a=0 ; a<links.length ; a++){
+      links[a].style.display = "none";
+    }
+    checker = false;
+  }
+}
