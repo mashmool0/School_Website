e4d794e4e24912195c28d54fc62457909442ab56
mani <manifallah052@gmail.com>
2024-06-10 08:48:33 +0330

    panel fixed

--- assets/JS/basket.js
+++ assets/JS/basket.js
@@ -6,7 +6,7 @@ function dropdown() {
       links[a].style.display = "block";
     }
     document.querySelector(".dropDown").style.marginTop = "120px";
-    document.querySelector(".notif").style.marginTop = "120px"
+    document.querySelector(".notif").style.marginTop = "120px";
     dropCheck = true;
   } else {
     let links = document.querySelectorAll(".dropDown div");
@@ -14,7 +14,6 @@ function dropdown() {
       links[a].style.display = "none";
     }
     document.querySelector(".dropDown").style.marginTop = "0";
-    document.querySelector(".notif").style.marginTop = "0"
     dropCheck = false;
   }
 }
@@ -25,7 +24,7 @@ function dropdown2() {
     for (let a = 0; a < links.length; a++) {
       links[a].style.display = "block";
     }
-    document.querySelector(".mob").style.marginTop = "120px";
+    document.querySelector(".mob").style.marginTop = "200px";
     dropCheck2 = true;
   } else {
     let links = document.querySelectorAll(".mob div");
@@ -36,29 +35,23 @@ function dropdown2() {
     dropCheck2 = false;
   }
 }
-plusBtn = document.querySelectorAll(".PlusBtn");
-for(let i=0 ; i<plusBtn.length ; i++){
-  plusBtn[i].addEventListener("click" , function(){
-    if(plusBtn[i].nextElementSibling.style.display == "none"){
-      plusBtn[i].nextElementSibling.style.display ="block"
-    }
-    else{
-      p = plusBtn[i].nextElementSibling.innerHTML;
-      plusBtn[i].nextElementSibling.innerHTML = ++p;
-    }
-  })
+plusBtn = document.querySelectorAll(".add-btn");
+for (let i = 0; i < plusBtn.length; i++) {
+  plusBtn[i].addEventListener("click", function () {
+    p = plusBtn[i].parentNode.previousElementSibling.innerHTML;
+    plusBtn[i].parentNode.previousElementSibling.innerHTML = ++p;
+  });
 }
 
-MinusBtn = document.querySelectorAll(".MinusBtn");
-for(let i=0 ; i<MinusBtn.length ; i++){
-  MinusBtn[i].addEventListener("click" , function(){
-    p = plusBtn[i].nextElementSibling.innerHTML;
-    if(p!=1){
-      plusBtn[i].nextElementSibling.innerHTML = --p;
+MinusBtn = document.querySelectorAll(".remove-btn");
+for (let i = 0; i < MinusBtn.length; i++) {
+  MinusBtn[i].addEventListener("click", function () {
+    p = MinusBtn[i].parentNode.previousElementSibling.innerHTML;
+    if (p == 1) {
+      document.getElementById("del").click();
     }
-    else if(p==1){
-      plusBtn[i].nextElementSibling.style.display = "none";
-
+    else{
+      MinusBtn[i].parentNode.previousElementSibling.innerHTML = --p;
     }
-  })
+  });
 }
