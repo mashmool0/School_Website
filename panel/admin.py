01f9372a72b4c7c43022823cb73932f76554f6b1
mashmool0 <ap4games2017@gmail.com>
2024-06-12 17:43:24 +0330

    change search field for admin in basket admin user

--- panel/admin.py
+++ panel/admin.py
@@ -10,3 +10,7 @@ admin.site.register(UserCourse)
 @admin.register(Basket)
 class BasketAdmin(admin.ModelAdmin):
     list_display = ['course_name', "teacher", "basket_user", 'price', "price_with_off"]
+    search_fields = ['basket_user__user_student__phone_number', 'basket_user__user_student__first_name',
+                     'basket_user__user_student__last_name']
+
+    list_filter = ('course_name', "teacher")
