ae6917aff44e7f93bdba28be025c84fe17b40469
mashmool0 <ap4games2017@gmail.com>
2024-06-16 19:47:45 +0330

    sorage for save data

--- SchoolSiteProject/models.py
+++ SchoolSiteProject/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 
 
 class Storage(models.Model):
     my_file = models.FileField()
+
+
+path = default_storage.save('/example.txt', ContentFile(b'Storage'))
